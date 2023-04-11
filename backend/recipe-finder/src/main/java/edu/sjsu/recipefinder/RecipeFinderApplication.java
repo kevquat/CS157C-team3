fe79e2cb0151895557891730008e999520e734ee
anushree <anushreeshah24@gmail.com>
2023-04-10 19:03:12 -0700

    Integrated ChatGPT with Java

--- backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/RecipeFinderApplication.java
+++ backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/RecipeFinderApplication.java
@@ -1,5 +1,6 @@
 package edu.sjsu.recipefinder;
 
+import edu.sjsu.recipefinder.service.DataLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class RecipeFinderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeFinderApplication.class, args);
+
+        new DataLoader();
     }
 
 }
