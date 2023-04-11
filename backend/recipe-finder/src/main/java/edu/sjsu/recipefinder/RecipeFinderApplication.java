8be31bd5f9c960c2981021db60bd789c105ab3bd
anushree <anushreeshah24@gmail.com>
2023-04-11 02:45:59 -0700

    Loaded data into DB

--- backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/RecipeFinderApplication.java
+++ backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/RecipeFinderApplication.java
@@ -3,6 +3,7 @@ package edu.sjsu.recipefinder;
 import edu.sjsu.recipefinder.service.DataLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class RecipeFinderApplication {
@@ -10,7 +11,15 @@ public class RecipeFinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeFinderApplication.class, args);
 
-        new DataLoader();
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server successfully");
+        System.out.println("Server is running: "+jedis.ping());
+
+        jedis.set("recipe-count","0");
+        //jedis.incr("recipe-count");
+        //System.out.println(jedis.get("recipe-count"));
+        new DataLoader().loadData(jedis);
+        }
+
     }
 
-}
